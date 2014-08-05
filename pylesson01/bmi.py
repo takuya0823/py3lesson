4f373ac0f30fb93bc74e35bf295cab3d0bfbfde9
Takuya SASAKI <takuya@zuozuomu.me>
2014-08-05 17:42:02 +0900

    chapter05 seciton05 step03 comp. chapter05 section06 comp.

---
+++ pylesson01/bmi.py
@@ -0,0 +1,19 @@
+# coding: Shift_JIS
+__author__ = 'ts'
+
+while True:
+    height = input('身長(ｍ)?:')           #組み込み関数raw_input
+    if len(height) == 0:
+        break
+    height = float(height)                      #小数に変換
+    weight = float(input('体重(kg)?:'))   #組み込み関数raw_input
+    bmi = weight/pow(height,2)                  #組み込み関数powで累乗計算
+    print('BMI値は%0.1fです。' % bmi)         #小数点第一位までの出力
+    if bmi < 18.5:
+        print('少し痩せすぎです')
+    elif 18.5 <= bmi < 25.0:
+        print('標準的な体型です')
+    elif 25.0<= bmi < 30:
+        print('少し太っています')
+    else:
+        print('高度の肥満です')
---
+++ pylesson01/ch05ss05st3.py
@@ -0,0 +1,11 @@
+__author__ = 'ts'
+import sys
+try:
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
+    print(a+b)
+except:
+    print('Error!')
+
+print('End')
+
